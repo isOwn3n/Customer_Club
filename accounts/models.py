9bcece956acb63c153c24a10581c4fb559dce96a
ownen <mr.joker.mr.joke@gmail.com>
2024-05-23 22:59:43 +0330

    add created_at, updated_at and deleted_at to Group model
    add some validators for phone_number and validate phone number

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.core.validators import (
+    RegexValidator,
+    MinLengthValidator,
+    MaxLengthValidator,
+)
 
 
 class Group(models.Model):
     group_english_name = models.CharField(max_length=100)
     group_persian_name = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         db_table = "customer_groups"
@@ -26,10 +35,12 @@ class CustomerManager(models.Manager):
     ):
         if not firstname:
             raise ValueError("Customers must have a firstname")
+
         if not lastname:
             raise ValueError("Customers must have a lastname")
-        if not points:
-            raise ValueError("Customers must have a point")
+
+        if not phone_number:
+            raise ValueError("Customers must have a phone_number")
 
         customer = self.model(
             firstname=firstname,
@@ -39,33 +50,40 @@ class CustomerManager(models.Manager):
             wedding_date=wedding_date,
             phone_number=phone_number,
         )
-        # customer = Customer.objects.create(
-        #     firstname=firstname,
-        #     lastname=lastname,
-        #     points=points,
-        #     birthday=birthday,
-        #     wedding_date=wedding_date,
-        #     phone_number=phone_number,
-        # )
         customer.save()
         for group in member_of:
             try:
-                # print("Group: ", Group.objects.get(**group))
-                # data = Group.objects.get(**group)
-                # print("Data: ", data)
                 customer.member_of.add(group)
-            except Exception as e:
-                print("Error: ", e)
+            except:
+                ...
 
         return customer
+
+
+phone_regex = RegexValidator(
+    regex=r"^(?:\+98|0)9\d{9}$", message="Invalid Phone Number"
+)
+
+phone_min_length = MinLengthValidator(
+    11, "Phone number is too short, it must be 11 chars."
+)
+phone_max_length = MaxLengthValidator(
+    11, "Phone number is too long, it must be 11 chars."
+)
 
 
 class Customer(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=11)
+    phone_number = models.CharField(
+        validators=[
+            phone_regex,
+            phone_min_length,
+            phone_max_length,
+        ],
+    )
     member_of = models.ManyToManyField(Group)
-    points = models.PositiveIntegerField(default=0)
+    points = models.PositiveIntegerField(null=True, blank=True)
     birthday = models.DateField(blank=True, null=True)
     wedding_date = models.DateField(blank=True, null=True)
 
