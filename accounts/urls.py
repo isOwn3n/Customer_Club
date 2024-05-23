480d298f18e9ef8e253a6930d55cd634fe9b99d0
ownen <mr.joker.mr.joke@gmail.com>
2024-05-23 23:02:18 +0330

    add group url and change token/ to login/ and token/blacklist/ to logout/

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,18 +3,20 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
+    TokenBlacklistView,
 )
 from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register("customer", views.CustomerView)
-
+router.register("customer", views.CustomerViewSet)
+router.register("group", views.GroupViewSet)
 
 
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("te/", include(router.urls, )),
+    path("te/", include(router.urls)),
 ]
