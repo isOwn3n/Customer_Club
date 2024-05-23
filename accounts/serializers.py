5986b93351749048784c100a9e17c8d01df59cf5
ownen <mr.joker.mr.joke@gmail.com>
2024-05-23 23:01:49 +0330

    write a serializer for Group write another serializer for customer to just get data of customer (member_of is string not id of group) and for creating a new customer member_ofs deleted_at should be null

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,27 +2,55 @@ from rest_framework import serializers
 from . import models
 
 
-class CustomerSerializer(serializers.ModelSerializer):
-    """A serializes for customers"""
-
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Customer
-        fields = (
-            "id",
-            "firstname",
-            "lastname",
-            "phone_number",
-            "member_of",
-            "points",
-            "birthday",
-            "wedding_date",
-        )
+        model = models.Group
         extra_kwargs = {
             "id": {
                 "read_only": True,
             },
         }
+        exclude = ("deleted_at",)
+
+
+class CustomerGetSerializer(serializers.ModelSerializer):
+    """A serializes for get customers data"""
+
+    member_of = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.Customer
+        extra_kwargs = {
+            "id": {
+                "read_only": True,
+            },
+        }
+        exclude = ("deleted_at",)
+
+    def get_member_of(self, obj):
+        return obj.member_of.values_list("group_persian_name", flat=True)
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    """A serializes for get customers data"""
+
+    member_of = serializers.PrimaryKeyRelatedField(
+        queryset=models.Group.objects.filter(deleted_at__isnull=True),
+        many=True
+    )
+
+
+    class Meta:
+        model = models.Customer
+        extra_kwargs = {
+            "id": {
+                "read_only": True,
+            },
+        }
+        exclude = ("deleted_at",)
 
     def create(self, validated_data):
-        customer = models.Customer.objects.create_customer(**validated_data)
+        if validated_data["points"] == None:
+            validated_data["points"] = 0
+        customer = models.Customer.objects.create_customer(**validated_data)  # type: ignore
         return customer
