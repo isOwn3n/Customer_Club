dc11b95391985cc313cd47c137c6942dcfe1650e
ownen <mr.joker.mr.joke@gmail.com>
2024-05-27 19:04:06 +0330

    create built-in messages to store scheduled messages like birthday and wedding date. create send log to save count of messages and the group that sent, to can handle a chart(In Frontend). create action model to save all actions done by users.

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+from customers.models import Group
+
+
+class BuiltInMessage(models.Model):
+    message = models.CharField(max_length=900)
+    is_birthday = models.BooleanField(default=False)
+    is_wedding_date = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def delete(self, *args, **kwargs):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def restore(self):
+        self.deleted_at = None
+        self.save()
+
+    def is_deleted(self):
+        return self.deleted_at is not None
+
+    class Meta:
+        db_table = "messages"
+        # Add an index to speed up queries for non-deleted objects
+        indexes = [
+            models.Index(fields=["deleted_at"]),
+        ]
+
+
+# This Model used for create chart of sent messages
+class SendLog(models.Model):
+    count = models.PositiveIntegerField()
+    gruops = models.ManyToManyField(Group)
+
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.created_at.year}-{self.created_at.month}-{self.created_at.day} ({self.count})"
+
+    class Meta:
+        db_table = "message_logs"
+
+
+ACTION_CHOICES = (
+    ("CREATE_CUSTOMER", "created a new customer"),
+    ("CREATE_USER", "created a new user"),
+    ("CREATE_GROUP", "created a new group for customers"),
+    ("CREATE_MESSAGE", "created a new message for birthday or wedding day"),
+    ("DELETE_CUSTOMER", "deleted a customer"),
+    ("DELETE_USER", "deleted a user"),
+    ("DELETE_GROUP", "deleted a group"),
+    ("DELETE_MESSAGE", "deleted one of the built in messages"),
+    ("UPDATE_CUSTOMER", "updated one of the customers"),
+    ("UPDATE_USER", "updated one of the users"),
+    ("UPDATE_GROUP", "updated one of the groups"),
+    ("UPDATE_MESSAGE", "updated one of the messages"),
+)
+
+
+class Action(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    action = models.CharField(max_length=255, choices=ACTION_CHOICES)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.action} ({self.user.username})"
+
+    def save(self, *args, **kwargs):
+        # Prevent updating existing instances
+        if self.pk:
+            raise ValueError("Updating existing instances is not allowed")
+        return super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        raise ValueError("Deleting instances is not allowed")
+
+    class Meta:
+        db_table = "actions"
