c4d83ca212d189fd318d8a2dcfb9b550e465288c
ownen <mr.joker.mr.joke@gmail.com>
2024-05-28 13:17:28 +0330

    this function used to dont import all send functions, just with one function can use all functions

---
+++ messaging/utils/send_message.py
@@ -0,0 +1,9 @@
+from send import sending
+
+
+def general_sending_message(message: str, customers_id: list[int]):
+    if len(customers_id) > 1:
+        if "%fullname" in message or "%name" in message or "%lastname" in message:
+            return sending.send_array_of_messages(message, customers_id)
+        return sending.send_one_message(message, customers_id)
+    return sending.send_single_message(message, customers_id[0])
