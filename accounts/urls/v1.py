a34e06091580feaf26099cbd326da923050aa2a6
ownen <mr.joker.mr.joke@gmail.com>
2024-06-03 14:35:21 +0330

    then change url to router

--- accounts/urls/v1.py
+++ accounts/urls/v1.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import include, path
 from accounts import views
+
+from rest_framework.routers import DefaultRouter
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -8,10 +10,13 @@ from rest_framework_simplejwt.views import (
     TokenBlacklistView,
 )
 
+router = DefaultRouter()
+router.register("me", views.MeViewSet)
+
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("me/", views.MeAPIView.as_view(), name="me"),
+    path("", include(router.urls)),
 ]
