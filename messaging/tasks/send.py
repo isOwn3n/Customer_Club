38586587c4a65d5892c7734b81f8e2d802886b84
ownen <mr.joker.mr.joke@gmail.com>
2024-06-21 16:59:39 +0330

    remove a sleep from sending message function in celery tasks

--- messaging/tasks/send.py
+++ messaging/tasks/send.py
@@ -4,9 +4,8 @@ from messaging.utils.send_message import general_sending_message
 
 
 @shared_task
-def send_message(message: str, customers_id: list[int]):
-    sleep(10)
-    return general_sending_message(message, customers_id).get("cost", 0)
+def send_message(message: str, customers_id: list[int]) -> dict[str, int]:
+    return general_sending_message(message, customers_id)
 
 
 @shared_task
